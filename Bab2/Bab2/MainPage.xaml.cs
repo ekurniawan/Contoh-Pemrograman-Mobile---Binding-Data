2cc7315eb6a18dcf2ec3bd1df3ff5eecea2d4e20
erick <erick@DESKTOP-7DP9Q9N>
2016-11-01 09:12:23 +0700

    menambahkan master page

--- Bab2/Bab2/MainPage.xaml.cs
+++ Bab2/Bab2/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace Bab2
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
---
+++ Bab2/Bab2/MasterPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Bab2.Models;
+
+namespace Bab2
+{
+    public partial class MasterPage : ContentPage
+    {
+        public ListView ListView { get { return listView; } }
+
+        public MasterPage()
+        {
+            InitializeComponent();
+
+            var masterPageItems = new List<MasterPageItem>();
+            masterPageItems.Add(new MasterPageItem
+            {
+                Title = "Daftar Barang",
+                IconSource = "icon.png",
+                TargetType = typeof(TampilBarangPage)
+            });
+            masterPageItems.Add(new MasterPageItem
+            {
+                Title = "Daftar List View",
+                IconSource = "icon.png",
+                TargetType = typeof(ListViewButton)
+            });
+
+            listView.ItemsSource = masterPageItems;
+        }
+    }
+}
